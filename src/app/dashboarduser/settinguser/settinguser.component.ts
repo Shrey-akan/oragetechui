e0b48cda8d61737a72b57f8ecd3e9f75e1bd5ff4
shrey_akan <shereyansjain0904@gmail.com>
2023-09-15 00:14:04 -0700

    footeradded

--- src/app/dashboarduser/settinguser/settinguser.component.ts
+++ src/app/dashboarduser/settinguser/settinguser.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-settinguser',
   templateUrl: './settinguser.component.html',
   styleUrls: ['./settinguser.component.css']
 })
-export class SettinguserComponent {
+export class SettinguserComponent implements OnInit {
+  
+  
+
+ 
+isOpen: boolean = false;
+  active: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleActive(id: number) {
+    this.active = id;
+  }
+
+  handleToggle() {
+    this.isOpen = !this.isOpen;
+  }
 
 }
